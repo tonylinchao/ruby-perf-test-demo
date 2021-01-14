05a4b72c5c10b54bb248a575ce2705573e4ddff3
Lin, Tony C <tony.c.lin@pccw.com>
2021-01-14 19:36:50 +0800

    Add fuse --> mule --> salesforce perf test case

--- src/test/scala/ruby.perf.test/BasicSimulation.scala
+++ src/test/scala/ruby.perf.test/BasicSimulation.scala
@@ -7,33 +7,42 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://localhost:8080/fusedemo") // Here is the root for all relative URLs
+    .baseUrl("https://ruby-fuse-demo-papi-ruby-fuse-ft-a.app3.osp.pccw.com/fusedemo") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   // MuleSoft VPC test
-  val scn_vpc = scenario("Test MuleSoft VPC").repeat(10) {  // millisecond  .repeat(100)  during(1000 * 60)
+  val scn_vpc = scenario("Scenario #1").repeat(100) {  // millisecond  .repeat(100)  during(1000 * 60)
     exec(http("MuleSoft VPC")
       .get("/vpc/test").check(status is 200)
     )
-  } 
+  }
 
+  // Get Salesforce contacts
+  val scn_sfdc_contacts = scenario("Scenario #2").repeat(1) {
+    exec(http("Get Salesforce contacts")
+      .get("/customers/salesforce-contacts").check(status is 200)
+    )
+  }
+
+
+  //setUp(scn_sfdc_contacts.inject(atOnceUsers(1)).protocols(httpProtocol))
+
+  setUp(scn_sfdc_contacts.inject(atOnceUsers(20), constantUsersPerSec(20).during(120.seconds)).protocols(httpProtocol))
+
+
+  // Sample
+  //nothingFor(4.seconds), // 设置一段停止的时间
+  //atOnceUsers(10), // 立即注入一定数量的虚拟用户
+  //rampUsers(10).during(1.seconds), // 在指定时间内，设置一定数量逐步注入的虚拟用户
+  //constantUsersPerSec(20).during(15.seconds), // 定义一个在每秒钟恒定的并发用户数，持续指定的时间
+  //constantUsersPerSec(20).during(15.seconds).randomized, // 定义一个在每秒钟围绕指定并发数随机增减的并发，持续指定时间
+  //rampUsersPerSec(10).to(20).during(10.minutes), // 定义一个并发数区间，运行指定时间，并发增长的周期是一个规律的值
+  //rampUsersPerSec(10).to(20).during(10.minutes).randomized, // 定义一个并发数区间，运行指定时间，并发增长的周期是一个随机的值
+  //heavisideUsers(20).during(20.seconds) // 定义一个持续的并发，围绕和海维赛德函数平滑逼近的增长量，持续指定时间
 
   // setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
-
-  setUp(
-    scn_vpc.inject(
-      //nothingFor(4.seconds), // 设置一段停止的时间
-      atOnceUsers(10), // 立即注入一定数量的虚拟用户
-      rampUsers(10).during(1.seconds), // 在指定时间内，设置一定数量逐步注入的虚拟用户
-      //constantUsersPerSec(20).during(15.seconds), // 定义一个在每秒钟恒定的并发用户数，持续指定的时间
-      //constantUsersPerSec(20).during(15.seconds).randomized, // 定义一个在每秒钟围绕指定并发数随机增减的并发，持续指定时间
-      //rampUsersPerSec(10).to(20).during(10.minutes), // 定义一个并发数区间，运行指定时间，并发增长的周期是一个规律的值
-      //rampUsersPerSec(10).to(20).during(10.minutes).randomized, // 定义一个并发数区间，运行指定时间，并发增长的周期是一个随机的值
-      //heavisideUsers(20).during(20.seconds) // 定义一个持续的并发，围绕和海维赛德函数平滑逼近的增长量，持续指定时间
-    ).protocols(httpProtocol)
-  )
 
 }
